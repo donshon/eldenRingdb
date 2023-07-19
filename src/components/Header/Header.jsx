57062a1c27d0136e04e2a1be64d42e92cd397a4d
Shawn <shawnche35@gmail.com>
2023-07-18 22:29:51 -0700

    comment

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,2 +1,34 @@
-import React, {useState, useContext} from 'react'
+import React from 'react'
 import './Header.css'
+import {FaHome} from "react-icons/fa";
+import {Link, useNavigate} from 'react-router-dom';
+import {auth} from '../../config/firebaseConfig'
+import {useAuthState} from 'react-firebase-hooks/auth'
+import {signOut} from 'firebase/auth'
+//npm install firebase
+
+function Header() {
+    //get user data
+    const [user] = useAuthState(auth);
+
+    let navigate = useNavigate();
+
+  return (
+    <div className="header-container">
+        <FaHome onClick={()=>navigate('/')}/>
+        {
+            user?
+            <div>
+                <span className="username">
+                    {user.displayName}
+                </span>
+                <button className="auth-link" onClick={()=>signOut(auth)}>Logout</button>
+            </div>
+            :
+            <Link to="/auth" className="auth-link">Sign Up/Log in</Link>
+        }
+    </div>
+  )
+}
+
+export default Header
